351e6cabffb0f774544221b844c690fde5e50118
Madhumidha10 <madhu4achieve@gmail.com>
2022-02-07 08:47:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Counter } from './Counter';
 import { textAlign } from '@mui/system';
+import { Link,Switch,Route } from 'react-router-dom';
+import useWindowSize from 'react-use/lib/useWindowSize';
+import Confetti from 'react-confetti';
 
 export default function App() {
  
@@ -71,50 +74,90 @@ export default function App() {
   
  
    return (
-    <div className='App'>
+  
+    // <div className='App'>
       
-   
-      <div className='itembg'>
-      <Typography component="legend" variant="h4" sx={{color:grey[900],textAlign:"center"}} >New Movie</Typography>
-      <TextField id="name" label="Enter the name..." variant="standard" onChange={e=>setName(e.target.value)}/> 
-       <TextField id="poster" label="Enter the URL..." variant="standard" onChange={e=>setPoster(e.target.value)}/> 
-       <TextField id="cast" label="Enter the Cast..." variant="standard" onChange={e=>{
-        
-         const actors =  e.target.value.split(',');
-         setCast(actors);
-}} /> 
-       <TextField id="summary" label="Enter the Summary..." variant="standard" onChange={e=>setSummary(e.target.value)} multiline/> 
-       <TextField id="rating"
-          label="Enter the Rating.."
-          type="number"
-          variant="standard"
-          onChange={e=>setRating(parseInt(e.target.value))}
-        />
-       <Button variant="contained"   onClick={()=>{const newMovie={ 
-            
-            name:name,
-            poster:poster,
-            cast:cast,
-            summary:summary,
-            rating:rating,
-            
-           };
-           
-        setMovieList([...movieList,newMovie]);
-       }}>Add Movie</Button>
-           </div>
-       <div className='Movie_container'> {movieList.map(({name,poster,cast,summary,rating},index)=><Movielist key={index} name={name} poster={poster} cast={cast} summary={summary} rating={rating} />)
-}</div> 
+     
 
-    </div>
+
+    // </div>
     
+    <div className='App'>
+       <ul>
+        <li>
+          <Link to="/movies">Movie</Link>
+        </li>
+        <li>
+          <Link to="/color-game">Color Game</Link>
+        </li>
+        <li>
+          <Link to="/tic-tac-toe">Tic Tac Toe</Link>
+        </li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+       
+      </ul>
+  
+
+    <Switch>
+      <Route path="/movies">
+              <div className='itembg'>
+  <Typography component="legend" variant="h4" sx={{color:grey[900],textAlign:"center"}} >New Movie</Typography>
+  <TextField id="name" label="Enter the name..." variant="standard" onChange={e=>setName(e.target.value)}/> 
+   <TextField id="poster" label="Enter the URL..." variant="standard" onChange={e=>setPoster(e.target.value)}/> 
+   <TextField id="cast" label="Enter the Cast..." variant="standard" onChange={e=>{
     
+     const actors =  e.target.value.split(',');
+     setCast(actors);
+}} /> 
+   <TextField id="summary" label="Enter the Summary..." variant="standard" onChange={e=>setSummary(e.target.value)} multiline/> 
+   <TextField id="rating"
+      label="Enter the Rating.."
+      type="number"
+      variant="standard"
+      onChange={e=>setRating(parseInt(e.target.value))}
+    />
+   <Button variant="contained"   onClick={()=>{const newMovie={ 
+        
+        name:name,
+        poster:poster,
+        cast:cast,
+        summary:summary,
+        rating:rating,
+        
+       };
+       
+    setMovieList([...movieList,newMovie]);
+   }}>Add Movie</Button>
+       </div>
+   <div className='Movie_container'> {movieList.map(({name,poster,cast,summary,rating},index)=><Movielist key={index} name={name} poster={poster} cast={cast} summary={summary} rating={rating} />)
+}</div> 
+      </Route>
+      <Route path="/color-game">
+        <Colorbox />
+      </Route>
+      <Route path="/tic-tac-toe">
+      <TicTacToe />
+      </Route>
+      <Route path="/">
+        <Msg />
+       
+      </Route>
+     
+    </Switch>
+  </div>
       
   
   
   );
 }
-
+function Msg()
+{
+  return <div>
+    <h4>Welcome to the Home Page</h4>;
+  </div>
+}
 function Movielist({name,poster,cast,summary,rating},index)
 {
   const styles={color:rating>7?"yellow":"white"}
@@ -159,4 +202,122 @@ function Movielist({name,poster,cast,summary,rating},index)
  
 
   );
+}
+
+function Colorbox()
+{
+  const [color,setColor]=useState("pink");
+  const styles={background:color};
+  const [colorList,setColorList]=useState(["red","Green","Blue",]);
+
+  return(<div className='ColorBox'>
+    <input style={styles} onChange={e=>setColor(e.target.value)} placeholder='Enter the color' value={color} />
+    <button onClick={()=>setColorList([...colorList,color])}>Add color</button>
+    {colorList.map((clr)=> 
+    (<Addcolor color={clr} />))}
+   
+   
+  </div>
+  );
+}
+function Addcolor({color})
+{
+  const styles={
+    backgroundColor:color,
+    height:"25px",width:"200px",marginTop:"10px"}
+  return (<div style={styles} ></div>);
+}
+
+function TicTacToe()
+{
+ 
+  const [board,setBoard]=useState([null,null,null,null,null,null,null,null,null]);
+ 
+ // draw
+const calculateDraw=(board) =>{
+  for (let i = 0; i < board.length; i++) {
+      if (board[i]===null) {
+          return false;
+      }
+  }
+  return true;
+};
+ 
+  
+  const decideWinner=(board)=>{const lines=[[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[1,4,7],[0,3,6],[2,5,8]];
+
+  
+  //if condition for winning
+  for(let i=0;i<lines.length;i++){
+    const [a,b,c]=lines[i];
+    if(board[a]!==null &&board[a]===board[b] &&board[b]===board[c]){
+      console.log("winner is",board[a])
+     // setWinner(board[a]);
+      return board[a];
+    }
+    
+    }
+    return null;
+  };
+
+  const winner= decideWinner(board);
+ const [isXTurn,setisXTurn]=useState(true); 
+
+  const handleClick=(index)=>
+  {
+    if(winner===null && board[index]===null)
+    {
+      const boardCopy=[...board];
+      boardCopy[index]= isXTurn?"X":"O";
+      setBoard(boardCopy);
+      setisXTurn(!isXTurn);
+      
+    }
+
+  }
+  
+
+  const { width, height } = useWindowSize();
+  return (
+  <div className='full-game'>
+
+    {/* //draw/tie logic */}
+       {calculateDraw(board)?<h2>Tie</h2>:""}
+    
+    {/* //player turn login */}
+    
+    {isXTurn?<h2>Player: X Turn</h2>:<h2>Player: O Turn</h2>}
+    {/* //restart logic */}
+    {/* //winnerlogic */}
+    {winner?<h2>Winner is :{winner}</h2>:""}
+    
+    <div className='board'>
+         {/* //winnerlogic */}
+       
+    {winner?<Confetti width={width} height={height} gravity={0.01} />:""}
+    {board.map((val,index)=>(<GameBox val={val} onPlayerClick={()=>handleClick(index)} />))}
+
+    <div className='choose-player'>
+         {/* <h2>Who start the game?</h2>
+         <input type="text" placeholder='type X or O' onchange={(e)=>setisXTurn(e.target.value)}/> */}
+       
+         </div>
+   
+    </div>
+    <button onClick={()=>setBoard([null,null,null,null,null,null,null,null,null])}> Restart</button>
+    
+  </div> );
+}
+
+function GameBox({val,onPlayerClick})
+{
+  // const [val,setVal]=useState("");
+  const styles={color:val==="X"?"green":"red"}
+  return <div onClick={()=>onPlayerClick()}
+  style={styles}
+  className="game-box"
+   >
+    {val}
+
+  </div>
 }
